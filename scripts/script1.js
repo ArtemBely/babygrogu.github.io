ae9b3adae25f31812f3cb4b4ee34db0c13c2f63f
Артем Белышев <you@example.com>
2022-07-30 23:05:37 +0200

    added desktop

--- scripts/script1.js
+++ scripts/script1.js
@@ -2,18 +2,25 @@ var discuss = document.querySelector('#discuss');
 var center = document.querySelector('#center');
 var content = document.querySelector('#content');
 var rightContent = document.querySelector('.rightContent');
+var smallFone = document.querySelector('#smallFone');
 discuss.addEventListener('click', () => {
   if(discuss.classList.contains('discussActive')) {
     discuss.classList.remove('discussActive');
     center.classList.remove('hideCentral');
     content.classList.remove('hideContent');
-    rightContent.style.backgroundImage = "url('images/Component2.png')";
+    rightContent.style.backgroundImage = "none";
+    smallFone.classList.add('displayBlock');
   }
   else {
     rightContent.style.backgroundImage = "url('images/Component1.png')";
+    smallFone.classList.remove('displayBlock');
     discuss.classList.add('discussActive');
     center.classList.add('hideCentral');
     content.classList.add('hideContent');
+  }
+  if(typeof window != "undefined" && window.innerWidth > 2700) {
+    smallFone.classList.remove('displayBlock');
+    rightContent.style.backgroundImage = "url('images/Component1.png')";
   }
 });
 var check = document.querySelector('#check');
@@ -36,4 +43,12 @@ selectField.addEventListener('change', () => {
   typeOfPlaceholder.placeholder = '@' :
   typeOfPlaceholder.placeholder = '';
   selectArrow.classList.remove('rotateArrow');
+});
+window.addEventListener('resize', () => {
+  if(typeof window != "indefined" && window.innerWidth > 2500) {
+    smallFone.style.width = '100%';
+  }
+  else {
+    smallFone.style.width = 'auto';
+  }
 });
